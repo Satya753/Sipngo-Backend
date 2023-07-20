1bc7ea8d71b1922ce92d9b17820a9d4e7c45dc0d
Satya753 <satyajeet753@gmail.com>
2023-07-20 17:46:26 +0530

    Db connectivity check

---
+++ models/DbInterface.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask , render_template , request , url_for , redirect
+
+import mysql.connector
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Helloworld@123'
+app.config['MYSQL_DB'] = 'sipngo'
+
+
+db = mysql.connector.connect(
+  host=os.getenv("DB_HOST"),
+  user=os.getenv("DB_USER"),
+  password=os.getenv("DB_PASSWORD"),
+  database=os.getenv("DB_NAME")
+)
+
+
+
+
+def getCategories():
+    cursor = db.cursor()
+    cursor.execute(''' SELECT * from category ''')
+    data = cursor.fetchall()
+    cursor.close()
+
+    return data
+
+
+print(getCategories())
+
